c79b0b7efe7a0e08b73461bf750599c36c0a83ff
anerirana <anerirana2000@gmail.com>
2021-06-13 22:38:45 +0530

    git repo restructure

--- src/OffensiveAudioClassifier/start_up.py
+++ src/OffensiveAudioClassifier/start_up.py
@@ -1,5 +1,9 @@
 # Store all audios filenames and labels
 # Convert all audio from speech to text and store
 # Get speech features for all audios
+# Data file, file_name, label, converted_text
 # Get text features for all audios
 # Final model
+
+
+# 1 Dense, 1 dropout, 1 Dense and 1 softmax
